a7481337c0dc49dd40d033781396ca26d4a4c1d9
Tanish <tanishj1711@gmail.com>
2025-11-24 13:36:17 +0530

    feat(UI): Home tab - Add age (auto updating) to the about me section

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ const syncPointer = ({ x: pointerX, y: pointerY }) => {
   document.documentElement.style.setProperty('--yp', yp)
 }
 document.body.addEventListener('pointermove', syncPointer)
+
+
+// Age calculation
+const dob = new Date('2010-11-17');
+
+function getAge(dob) {
+  const now = new Date();
+  let age = now.getFullYear() - dob.getFullYear();
+
+  // check if the dob has occured
+  const monthDiff = now.getMonth() - dob.getMonth();
+  const dayDiff = now.getDate() - dob.getDate();
+  
+  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+    age -= 1;
+  }
+
+  return age;
+}
+
+document.getElementById('age').textContent = getAge(dob) + 'yo';
